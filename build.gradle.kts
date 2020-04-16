6267e7d5bf3e4bd80a8f65df5d22f09844fa4772
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2020-04-16 16:31:13 +0300

    [build] Upgraded junit and improved kotlin-dsl in gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 
 plugins {
     java
+    id("info.solidsoft.pitest") version "1.4.8"
 }
 
 java {
@@ -10,20 +11,15 @@ java {
 }
 
 buildscript {
-    val pitest = configurations.maybeCreate("pitest")
+    val pitest by configurations.creating
     dependencies {
-        classpath("info.solidsoft.gradle.pitest:gradle-pitest-plugin:1.4.7")
         pitest("org.pitest:pitest-junit5-plugin:0.12")
     }
 }
 
-apply(plugin = "info.solidsoft.pitest")
-configure<PitestPluginExtension> {
+pitest {
     testPlugin.set("junit5")
-//    excludedClasses.set(setOf("com.att.training.demo.mutations.Calculator"))
-//    excludedTestClasses.set(setOf("com.att.training.demo.mutations.CalculatorTest"))
 }
-
 
 group = "com.att.training.demo"
 version = "1.0-SNAPSHOT"
@@ -37,7 +33,7 @@ dependencies {
     val mockitoVersion = "3.3.3"
     implementation("org.slf4j:slf4j-simple:1.7.30")
 
-    testImplementation("org.junit.jupiter:junit-jupiter:5.6.1")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.6.2")
     testImplementation("org.assertj:assertj-core:3.15.0")
     testImplementation("org.mockito:mockito-core:$mockitoVersion")
     testImplementation("org.mockito:mockito-junit-jupiter:$mockitoVersion")
