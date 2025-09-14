d9c8fd350c7dd6b11070c2a9a4efdc54533c1dcf
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2025-09-14 14:01:01 +0300

    Upgraded gradle to 7.3, junit to 5.8.2, mockito to 4.1.0, pit to 1.7.3. Customized the display name in the @ParameterizedTest

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 pitest {
-    pitestVersion.set("1.7.2")
+    pitestVersion.set("1.7.3")
     junit5PluginVersion.set("0.15")
 }
 
@@ -19,9 +19,9 @@ repositories {
 
 dependencies {
     implementation("org.slf4j:slf4j-simple:1.7.32")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
     testImplementation("org.assertj:assertj-core:3.21.0")
-    val mockitoVersion = "4.0.0"
+    val mockitoVersion = "4.1.0"
     testImplementation("org.mockito:mockito-core:$mockitoVersion")
     testImplementation("org.mockito:mockito-junit-jupiter:$mockitoVersion")
 }
