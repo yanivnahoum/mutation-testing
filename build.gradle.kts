d62c084f2aa666ed751e0cebdaa8ff4e79dd8f9c
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2025-09-14 14:30:15 +0300

    Update Gradle wrapper to version 9.0.0 and upgrade Pitest and Mockito dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,38 +1,47 @@
 plugins {
     java
-    id("info.solidsoft.pitest") version "1.9.0"
-    id("com.adarshr.test-logger") version "3.2.0"
+    id("info.solidsoft.pitest") version "1.19.0-rc.1"
+    id("com.adarshr.test-logger") version "4.0.0"
 }
 
 pitest {
-    pitestVersion.set("1.9.5")
-    junit5PluginVersion.set("1.0.0")
+    pitestVersion.set("1.20.2")
+    junit5PluginVersion.set("1.2.3")
 }
 
 group = "com.att.training.demo"
 version = "1.0-SNAPSHOT"
 
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
 
 repositories {
     mavenCentral()
 }
 
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+val mockitoAgent = configurations.create("mockitoAgent")
 dependencies {
-    implementation("org.slf4j:slf4j-simple:2.0.3")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.9.1")
-    testImplementation("org.assertj:assertj-core:3.23.1")
-    val mockitoVersion = "4.8.0"
-    testImplementation("org.mockito:mockito-core:$mockitoVersion")
+    implementation("org.slf4j:slf4j-simple:2.0.17")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.13.4")
+    testImplementation("org.assertj:assertj-core:3.27.4")
+    val mockitoVersion = "5.19.0"
+    mockitoAgent("org.mockito:mockito-core:$mockitoVersion") { isTransitive = false }
     testImplementation("org.mockito:mockito-junit-jupiter:$mockitoVersion")
 }
 
 tasks {
-    withType<JavaCompile>().configureEach {
-        options.release.set(17)
-    }
-
     test {
         useJUnitPlatform()
+        jvmArgs("-javaagent:${mockitoAgent.asPath}")
         testlogger {
             showStandardStreams = true
         }
