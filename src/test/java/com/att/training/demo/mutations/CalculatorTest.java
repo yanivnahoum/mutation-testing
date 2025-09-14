5da71d632699f4919a09219158d4122eb46c7aa5
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2025-09-14 15:46:19 +0300

    Remove unused import for assertEquals in CalculatorTest

--- src/test/java/com/att/training/demo/mutations/CalculatorTest.java
+++ src/test/java/com/att/training/demo/mutations/CalculatorTest.java
@@ -9,7 +9,6 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
