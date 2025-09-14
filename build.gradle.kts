fd543cc83e88a2e24cfc98d793c25df0f6f9e476
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2025-09-14 14:01:01 +0300

    Switched to java 17. Upgraded gradle to 7.4.2, maven wrapper to 3.8.6, mockito to 4.6.1, assertj to 1.23.1, pit to 1.9.0, junit5-pitest-plugin to 1.0.0.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
     java
-    id("info.solidsoft.pitest") version "1.7.0"
-    id("com.adarshr.test-logger") version "3.1.0"
+    id("info.solidsoft.pitest") version "1.7.4"
+    id("com.adarshr.test-logger") version "3.2.0"
 }
 
 pitest {
-    pitestVersion.set("1.7.3")
-    junit5PluginVersion.set("0.15")
+    pitestVersion.set("1.9.0")
+    junit5PluginVersion.set("1.0.0")
 }
 
 group = "com.att.training.demo"
@@ -18,17 +18,17 @@ repositories {
 }
 
 dependencies {
-    implementation("org.slf4j:slf4j-simple:1.7.32")
+    implementation("org.slf4j:slf4j-simple:1.7.36")
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
-    testImplementation("org.assertj:assertj-core:3.21.0")
-    val mockitoVersion = "4.1.0"
+    testImplementation("org.assertj:assertj-core:3.23.1")
+    val mockitoVersion = "4.6.1"
     testImplementation("org.mockito:mockito-core:$mockitoVersion")
     testImplementation("org.mockito:mockito-junit-jupiter:$mockitoVersion")
 }
 
 tasks {
     withType<JavaCompile>().configureEach {
-        options.release.set(11)
+        options.release.set(17)
     }
 
     test {
