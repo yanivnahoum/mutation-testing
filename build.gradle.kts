8b80be44497bea94505b49c624f37d44bb9047b3
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2025-09-14 14:01:01 +0300

    Upgraded dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
     java
-    id("info.solidsoft.pitest") version "1.7.4"
+    id("info.solidsoft.pitest") version "1.9.0"
     id("com.adarshr.test-logger") version "3.2.0"
 }
 
 pitest {
-    pitestVersion.set("1.9.2")
+    pitestVersion.set("1.9.5")
     junit5PluginVersion.set("1.0.0")
 }
 
@@ -18,10 +18,10 @@ repositories {
 }
 
 dependencies {
-    implementation("org.slf4j:slf4j-simple:1.7.36")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
+    implementation("org.slf4j:slf4j-simple:2.0.0")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
     testImplementation("org.assertj:assertj-core:3.23.1")
-    val mockitoVersion = "4.6.1"
+    val mockitoVersion = "4.7.0"
     testImplementation("org.mockito:mockito-core:$mockitoVersion")
     testImplementation("org.mockito:mockito-junit-jupiter:$mockitoVersion")
 }
