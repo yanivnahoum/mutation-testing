5d513b1d987c4db26b37f7ad6a674234d6881128
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2025-09-14 14:00:59 +0300

    [build] Upgraded gradle & maven version and project dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,14 +12,14 @@ java {
 buildscript {
     val pitest = configurations.maybeCreate("pitest")
     dependencies {
-        classpath("info.solidsoft.gradle.pitest:gradle-pitest-plugin:1.4.0")
-        pitest("org.pitest:pitest-junit5-plugin:0.9")
+        classpath("info.solidsoft.gradle.pitest:gradle-pitest-plugin:1.4.5")
+        pitest("org.pitest:pitest-junit5-plugin:0.10")
     }
 }
 
 apply(plugin = "info.solidsoft.pitest")
 configure<PitestPluginExtension> {
-    testPlugin = "junit5"
+    testPlugin.set("junit5")
 }
 
 group = "com.att.training.demo"
@@ -32,7 +32,7 @@ repositories {
 
 dependencies {
     val mockitoVersion = "3.1.0"
-    implementation("org.slf4j:slf4j-simple:1.7.28")
+    implementation("org.slf4j:slf4j-simple:1.7.29")
     testImplementation("org.junit.jupiter:junit-jupiter:5.5.2")
     testImplementation("org.assertj:assertj-core:3.13.2")
     testImplementation("org.mockito:mockito-core:$mockitoVersion")
