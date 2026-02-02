a6f2796e35677c3f7b0d76beeebd9f99333c695f
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2026-02-02 18:35:33 +0200

    Update build configurations and dependencies for Maven and Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
     java
     jacoco
-    id("info.solidsoft.pitest") version "1.19.0-rc.1"
+    id("info.solidsoft.pitest") version "1.19.0-rc.2"
     id("com.adarshr.test-logger") version "4.0.0"
 }
 
 pitest {
-    pitestVersion.set("1.20.2")
+    pitestVersion.set("1.22.0")
     junit5PluginVersion.set("1.2.3")
 }
 
@@ -26,15 +26,15 @@ repositories {
 val mockitoAgent = configurations.create("mockitoAgent")
 dependencies {
     implementation("org.slf4j:slf4j-simple:2.0.17")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.13.4")
-    testImplementation("org.assertj:assertj-core:3.27.4")
-    val mockitoVersion = "5.19.0"
+    testImplementation("org.junit.jupiter:junit-jupiter:6.0.2")
+    testImplementation("org.assertj:assertj-core:3.27.7")
+    val mockitoVersion = "5.21.0"
     mockitoAgent("org.mockito:mockito-core:$mockitoVersion") { isTransitive = false }
     testImplementation("org.mockito:mockito-junit-jupiter:$mockitoVersion")
 }
 
 jacoco {
-    toolVersion = "0.8.13"
+    toolVersion = "0.8.14"
 }
 val jacocoTestReport = tasks.jacocoTestReport
 tasks {
